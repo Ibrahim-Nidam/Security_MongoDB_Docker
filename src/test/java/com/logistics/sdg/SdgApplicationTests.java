d7959b4db0218b532adb65e4ec89f889e8197dff
Ibrahim Nidam <ibrahim.nidam22@gmail.com>
2025-11-20 14:26:27 +0100

    test(fix) : small test to fix jenkins

--- src/test/java/com/logistics/sdg/SdgApplicationTests.java
+++ src/test/java/com/logistics/sdg/SdgApplicationTests.java
@@ -3,10 +3,24 @@ package com.logistics.sdg;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.MongoDBContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest
 @ActiveProfiles("test")
+@Testcontainers
 class SdgApplicationTests {
+
+    @Container
+    static MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo:7.0");
+
+    @DynamicPropertySource
+    static void setProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.data.mongodb.uri", mongoDBContainer::getReplicaSetUrl);
+    }
 
     @Test
     void contextLoads() {
